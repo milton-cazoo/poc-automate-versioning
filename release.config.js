0fe3b3f7840629cfd5032242e91fb51cb58dd92a
Milton Boos Junior <ext_milton.junior@cazoo.co.uk>
2022-04-07 15:05:42 +0100

    chore: update repository field to ssh

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  branches: ["main"],
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
+};
